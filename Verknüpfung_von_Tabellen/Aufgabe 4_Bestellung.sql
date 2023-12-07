e2fa3fff5fcb07f144c5fa9526fb4f79970a655c
Lukas Belz <belz@singhammer.com>
2023-12-07 14:25:57 +0100

    Aufgabe 4 add

---
+++ Verknüpfung_von_Tabellen/Aufgabe 4_Bestellung.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT m.id, m.name, m.vorname  FROM mitarbeiter m
+JOIN bestellung b ON m.id = b.mid  JOIN kunde k ON b.kid = k.id
+JOIN ort o ON k.oid = o.id
+WHERE o.name = 'Greven'
+ORDER BY m.name ASC;
