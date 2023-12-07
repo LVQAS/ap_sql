5fa1fe9fd317a0d6a8af3aba595c02e65b238113
Lukas Belz <belz@singhammer.com>
2023-12-07 14:38:10 +0100

    Formatting fix

--- Verknüpfung_von_Tabellen/Aufgabe 4_Greven.sql
+++ Verknüpfung_von_Tabellen/Aufgabe 4_Greven.sql
@@ -1,5 +1,6 @@
 SELECT DISTINCT m.id, m.name, m.vorname  FROM mitarbeiter m
-JOIN bestellung b ON m.id = b.mid  JOIN kunde k ON b.kid = k.id
+JOIN bestellung b ON m.id = b.mid
+JOIN kunde k ON b.kid = k.id
 JOIN ort o ON k.oid = o.id
 WHERE o.name = 'Greven'
 ORDER BY m.name ASC;
