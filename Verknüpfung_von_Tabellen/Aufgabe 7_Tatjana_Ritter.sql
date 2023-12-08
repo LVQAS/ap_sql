a6cbe5617dfe00711c9a0cc28cfda5340f968dcf
LVQAS <123311437+LVQAS@users.noreply.github.com>
2023-12-08 12:32:43 +0100

    aufgabe 7 try

--- Verknüpfung_von_Tabellen/Aufgabe 7_Tatjana_Ritter.sql
+++ Verknüpfung_von_Tabellen/Aufgabe 7_Tatjana_Ritter.sql
@@ -1,9 +1,1 @@
-SELECT m.vorname, m.name FROM mitarbeiter m
-SELECT m.
-
-
-WHERE m.name = 'Hoffmann'
-
-SELECT * FROM funktion
-SELECT * FROM mitarbeiter
-SELECT * FROM abteilung
+SELECT vorname, name FROM mitarbeiter WHERE name = 'Hoffmann'
