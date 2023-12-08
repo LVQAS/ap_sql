48baa8818ebd95759ae00b39b64748eeb7d484bd
LVQAS <123311437+LVQAS@users.noreply.github.com>
2023-12-08 12:06:32 +0100

    Aufgabe 5

--- Verknüpfung_von_Tabellen/Aufgabe 5_Lieferanten_Bayern.sql
+++ Verknüpfung_von_Tabellen/Aufgabe 5_Lieferanten_Bayern.sql
@@ -1,9 +1,5 @@
 SELECT DISTINCT l.id, l.name FROM lieferant l
 JOIN ort o ON o.id = l.oid
-JOIN landkreis la ON la.id = o.id
-JOIN 
- 
-SELECT * FROM lieferant
-SELECT * FROM ort WHERE ID = 7889
-SELECT * FROM landkreis
-SELECT * FROM bundesland
+JOIN landkreis la ON la.id = o.lid
+JOIN bundesland b ON b.id = la.bid
+WHERE b.name = 'Bayern'
