d536cecab2a03fdaedce6b76cc3f6d57a741e6c7
LVQAS <123311437+LVQAS@users.noreply.github.com>
2023-12-08 21:13:39 +0100

    Aufgabe 7

--- Verknüpfung_von_Tabellen/Aufgabe 7_Tatjana_Ritter.sql
+++ Verknüpfung_von_Tabellen/Aufgabe 7_Tatjana_Ritter.sql
@@ -1,1 +1,4 @@
-SELECT vorname, name FROM mitarbeiter WHERE name = 'Hoffmann'
+SELECT m.vorname, m.name FROM mitarbeiter m
+JOIN abteilung a ON m.id = a.lid
+JOIN mitarbeiter n ON n.aid = a.id
+WHERE n.name = 'Ritter' AND n.vorname = 'Tatjana'
